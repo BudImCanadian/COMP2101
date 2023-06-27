b8e27961dec02dfcc8b1d4f61c05ae56302dbc49
BudImCanadian <133236920+BudImCanadian@users.noreply.github.com>
2023-06-26 23:11:09 -0400

    Add files via upload

---
+++ bash/args.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat <<EOF
+the variable 1 has '$1' in it
+the variable 2 has '$2' in it
+the variable 3 has '$3' in it
+the variable 0 has '$0' in it
+
+So the command that was used to run this command was args.sh
+
+interesting, innit?
+EOF
